2851027919428b99ea6fc9210649007872a8ee72
Gonzalo Garcia <gonzalohernang8@gmail.com>
2023-09-22 18:21:52 -0600

    Segundo Commit metodos

---
+++ PYTHON CLASE UNO/metodos/metodos-de-cadena.py
@@ -0,0 +1,11 @@
+
+#DEFINIR CADENAS
+
+cadena_uno = "Hola, mundo"
+cadena_dos = "Gonzalo Hernán"
+cadena_tres = "manzana dura"
+
+#METODOS 
+
+longitud = len(cadena_uno) #Devuelve el número de caracteres en la cadena.
+mayusculas = cadena_uno.upper() 
